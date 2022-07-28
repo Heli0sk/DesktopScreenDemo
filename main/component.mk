6c0ecca21f946dd7837262fd4505f37d951f7c66
leo <kzyready@163.com>
2022-07-28 01:01:16 +0000

    add http server html file

--- main/component.mk
+++ main/component.mk
@@ -2,3 +2,6 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_EMBED_FILES := html/favicon.ico
+COMPONENT_EMBED_FILES += html/setting.html
