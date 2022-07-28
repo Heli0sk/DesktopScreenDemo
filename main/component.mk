129e04e4e81fdfa8da43bce0796a27b68baddb8d
leo <kzyready@163.com>
2022-07-28 01:08:30 +0000

    https

--- main/component.mk
+++ main/component.mk
@@ -5,3 +5,5 @@
 
 COMPONENT_EMBED_FILES := html/favicon.ico
 COMPONENT_EMBED_FILES += html/setting.html
+COMPONENT_EMBED_TXTFILES := https_pem/server_root_cert.pem
+COMPONENT_EMBED_TXTFILES += https_pem/server_root_cert_bilibili.pem
